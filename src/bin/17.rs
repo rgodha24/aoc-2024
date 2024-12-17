04e325cc69828a44a6f86d32044f3db308c24511
Rohan Godha <git@rohangodha.com>
2024-12-17 00:42:55 -0700

    brute forcing isnt working :(

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,84 +1,103 @@
 advent_of_code::solution!(17);
 use std::str::FromStr;
 
-use advent_of_code::helpers::*;
+use indicatif::{ParallelProgressIterator, ProgressStyle};
 use itertools::Itertools;
+use rayon::prelude::*;
 
 #[derive(Clone, Copy, Debug)]
-enum Instruction {
-    Adv,
-    Bxl,
-    Bst,
-    Jnz,
-    Bxc,
-    Out,
-    Bdv,
-    Cdv,
-}
-
-#[derive(Clone, Debug)]
 struct Computer {
     a: usize,
     b: usize,
     c: usize,
-    instructions: Vec<(Instruction, usize)>,
+    instructions: (u64, usize),
 }
 impl Computer {
-    fn solve(self) -> Vec<usize> {
+    fn solve(&self) -> (u64, usize) {
         let Computer {
             mut a,
             mut b,
             mut c,
             instructions,
         } = self;
+        let (instructions, count) = *instructions;
 
         let mut i = 0;
-        let mut output = Vec::new();
-        while i < instructions.len() {
-            let (instruction, literal) = instructions[i].clone();
-            let combo = match literal {
-                0..=3 => literal,
+        let mut output = 0u64;
+        let mut n = 0;
+        while i < count {
+            let instr = (instructions >> ((count - i - 2) * 3)) & 0b111111;
+            let (instr, literal) = (instr >> 3, instr & 0b111);
+            let combo = move || match literal {
+                n @ 0..=3 => n as usize,
                 4 => a,
                 5 => b,
                 6 => c,
-                _ => usize::MAX,
+                // we just assume we're not gonna use this lol
+                n => n as usize,
             };
-            match instruction {
-                Instruction::Adv => a = a / (1 << combo),
-                Instruction::Bxl => b = b ^ literal,
-                Instruction::Bst => b = combo & 0b111,
-                Instruction::Jnz => {
+            match instr {
+                // Adv
+                0 => a = a / (1 << combo()),
+                // Bxl
+                1 => b = b ^ (literal as usize),
+                // Bst
+                2 => b = combo() & 0b111,
+                // Jnz
+                3 => {
                     if a != 0 {
-                        assert!(literal % 2 == 0);
-                        if i != literal as usize / 2 {
-                            i = literal as usize / 2;
+                        let n = literal as usize;
+                        if i != n {
+                            i = n;
                             continue;
                         }
                     }
                 }
-                Instruction::Bxc => {
+                // Bxc
+                4 => {
                     b = b ^ c;
                 }
-                Instruction::Out => {
-                    output.push(combo & 0b111);
+                // Out
+                5 => {
+                    output = (output << 3) | (combo() as u64 & 0b111);
+                    n += 1;
                 }
-                Instruction::Bdv => b = a / (1 << combo),
-                Instruction::Cdv => c = a / (1 << combo),
+                // Bdv
+                6 => b = a / (1 << combo()),
+                // Cdv
+                7 => c = a / (1 << combo()),
+                _ => unreachable!(),
             }
 
-            i += 1;
+            i += 2;
         }
-        output
+        (output, n)
     }
 }
 
 pub fn part_one(input: &str) -> Option<String> {
     let computer: Computer = input.parse().unwrap();
-    Some(computer.solve().into_iter().join(","))
+    Some(display(computer.solve()))
 }
 
-pub fn part_two(input: &str) -> Option<i64> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let computer: Computer = input.parse().unwrap();
+
+    (10usize.pow(14)..10usize.pow(15))
+        .into_par_iter()
+        .progress_with_style(
+            ProgressStyle::with_template("{human_pos}/{len} {wide_bar} {eta_precise}")
+                .expect("valid progress bar"),
+        )
+        .find_first(|a| {
+            let mut computer = computer;
+            computer.a = *a;
+            computer.solve() == computer.instructions
+        })
+}
+
+fn display((output, n): (u64, usize)) -> String {
+    (0..n).rev().map(|i| (output >> i * 3) & 0b111).join(",")
 }
 
 impl FromStr for Computer {
@@ -91,18 +110,19 @@ impl FromStr for Computer {
             .map(|line| (line.split_once(": ").unwrap().1).parse().unwrap())
             .collect_tuple()
             .unwrap();
-        let instructions: Vec<(Instruction, usize)> = instructions[9..]
-            .trim()
-            .split(",")
-            .tuples()
-            .map(|(instr, n)| {
-                use Instruction::*;
-                (
-                    [Adv, Bxl, Bst, Jnz, Bxc, Out, Bdv, Cdv][(instr).parse::<usize>().unwrap()],
-                    n.parse().unwrap(),
-                )
-            })
-            .collect_vec();
+
+        let instructions =
+            instructions[9..]
+                .trim()
+                .split(",")
+                .fold((0, 0), |(instructions, count), n| {
+                    (
+                        (instructions << 3) | (n.parse::<u64>().unwrap() & 0b111),
+                        count + 1,
+                    )
+                });
+
+        println!("{}", display(instructions));
 
         Ok(Self {
             a,
@@ -179,6 +199,6 @@ Program: 4,0,5,5
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(117440));
     }
 }
