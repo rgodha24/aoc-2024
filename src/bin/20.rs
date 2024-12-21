298deb6710f83025af99c4b87373e024aad95e69
Rohan Godha <git@rohangodha.com>
2024-12-20 21:58:03 -0700

    Optimize day 20 (#6)
    * wip
    
    * done optimizing?
    
    * rm unused state struct
    
    * only cheat starting from empty tiles
    
    * fix tests
    
    * cleanup
    
    * fix weird perf regression

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,16 +1,14 @@
 advent_of_code::solution!(20);
-use std::{collections::BinaryHeap, fmt::Binary};
 
 use advent_of_code::helpers::*;
-use itertools::Itertools;
 
 tiles!('.' => Empty, '#' => Wall, 'S' => Start, 'E' => End);
 
-pub fn part_one(input: &str) -> Option<usize> {
-    let grid: Grid<Tile> = Grid::from_chars(input);
+fn solve(input: &str, cheat_distance: i64, threshold: usize) -> usize {
+    let grid: Grid<Tile, 142> = Grid::from_chars(input);
     let start = grid.find(Tile::Start).next().unwrap();
     let end = grid.find(Tile::End).next().unwrap();
-    let mut fastest: Grid<usize> = grid.empty_sized();
+    let mut fastest = grid.empty_sized();
     fastest.fill(usize::MAX);
     let mut q = vec![(0, start)];
 
@@ -20,131 +18,81 @@ pub fn part_one(input: &str) -> Option<usize> {
         }
         fastest[point] = cost;
         for neighbor in grid.neighbors_of(point) {
-            if grid[neighbor] != Tile::Wall {
+            if grid[neighbor] != Tile::Wall && fastest[neighbor] > cost + 1 {
                 q.push((cost + 1, neighbor));
             }
         }
     }
 
-    let start_to_end = fastest[end];
+    let ste = fastest[end];
 
-    Some(
-        grid.points()
-            .flat_map(|point| Direction::all().into_iter().map(move |d| (point, d)))
-            .flat_map(|(point, direction)| {
-                if grid[point] == Tile::Wall {
-                    return None;
-                }
-                match grid.get(point.as_signed_point() + direction + direction) {
-                    None | Some(Tile::Wall) => {
-                        // doesn't save us any time
-                        None
-                    }
-                    Some(_) => {
-                        let before_cheat_time = fastest[point];
-                        let after_cheat_time = fastest[point + direction + direction];
-
-                        if after_cheat_time > start_to_end {
-                            Some(
-                                (after_cheat_time - start_to_end)
-                                    + (before_cheat_time - start_to_end),
-                            )
-                        } else {
-                            (after_cheat_time > before_cheat_time)
-                                .then(|| (after_cheat_time - before_cheat_time))
-                        }
-                    }
-                }
+    grid.points()
+        .filter(|p| matches!(&grid[p.cast()], Tile::Empty | Tile::Start))
+        .flat_map(move |point| {
+            (-cheat_distance..=cheat_distance).flat_map(move |x| {
+                let max_y = cheat_distance - x.abs();
+                (-max_y..=max_y).map(move |y| {
+                    (
+                        point,
+                        point + SignedPoint::new(x, y),
+                        (y.abs() + x.abs()) as usize,
+                    )
+                })
             })
-            .map(|saved| saved - 2)
-            .filter(|saved| *saved >= 100)
-            .count(),
-    )
+        })
+        .filter_map(|(cheat_start, cheat_end, distance)| {
+            if grid[cheat_start.cast()] == Tile::Wall {
+                return None;
+            }
+            match grid.get(cheat_end) {
+                None | Some(Tile::Wall) => None,
+                Some(_) => {
+                    let bct = fastest[cheat_start.cast()];
+                    let act = fastest[cheat_end.cast()];
+
+                    if act > ste {
+                        // if we cheated to a spot further away from start than the distance, the
+                        // distance from us to the end is (act - ste), and the distance from the cheat
+                        // start point is (bct - ste). the sum of those 2 numbers, minus the
+                        // distance, is the amount of time we've saved with this cheat
+                        Some((act - ste) + (bct - ste) - distance)
+                    } else {
+                        // return how much time we saved as long as it's positive
+                        act.checked_sub(bct + distance)
+                    }
+                }
+            }
+        })
+        .filter(|saved| *saved >= threshold)
+        .count()
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    Some(solve(input, 2, 100))
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let grid: Grid<Tile> = Grid::from_chars(input);
-    let start = grid.find(Tile::Start).next().unwrap();
-    let end = grid.find(Tile::End).next().unwrap();
-    let mut fastest: Grid<usize> = grid.empty_sized();
-    fastest.fill(usize::MAX);
-    let mut q = vec![(0, start)];
-
-    while let Some((cost, point)) = q.pop() {
-        if fastest[point] <= cost {
-            continue;
-        }
-        fastest[point] = cost;
-        for neighbor in grid.neighbors_of(point) {
-            if grid[neighbor] != Tile::Wall {
-                q.push((cost + 1, neighbor));
-            }
-        }
-    }
-
-    let start_to_end = fastest[end];
-
-    Some(
-        dbg!(grid
-            .points()
-            .flat_map(|point| {
-                (-20..=20i64).flat_map(move |x| {
-                    (-20..=20i64).filter_map(move |y| {
-                        let distance = x.abs() + y.abs();
-                        (distance <= 20).then_some((
-                            point,
-                            point + SignedPoint::new(x, y),
-                            distance as usize,
-                        ))
-                    })
-                })
-            })
-            .flat_map(|(cs, ce, distance)| {
-                if grid[cs.cast()] == Tile::Wall {
-                    return None;
-                }
-                match grid.get(ce) {
-                    None | Some(Tile::Wall) => {
-                        // doesn't save us any time
-                        None
-                    }
-                    Some(_) => {
-                        let before_cheat_time = fastest[cs.cast()];
-                        let after_cheat_time = fastest[ce.cast()];
-
-                        if after_cheat_time > start_to_end {
-                            Some(
-                                (after_cheat_time - start_to_end)
-                                    + (before_cheat_time - start_to_end)
-                                    - distance,
-                            )
-                        } else {
-                            (after_cheat_time > before_cheat_time)
-                                .then(|| (after_cheat_time - before_cheat_time - distance))
-                        }
-                    }
-                }
-            })
-            .filter(|saved| *saved >= 100)
-            .counts())
-        .values()
-        .sum(),
-    )
+    Some(solve(input, 20, 100))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::*;
 
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(0));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    #[rstest]
+    #[case(2, 1, 44)] // given in problem statement
+    #[case(2, 20, 5)]
+    #[case(2, 25, 4)]
+    #[case(20, 25, 1027)]
+    #[case(20, 40, 593)]
+    #[case(20, 50, 285)] // given in problem statement
+    #[case(20, 60, 129)]
+    #[case(20, 70, 41)]
+    #[case(20, 80, 0)]
+    fn test_day_20(#[case] distance: i64, #[case] threshold: usize, #[case] amount: usize) {
+        let s = &advent_of_code::template::read_file("examples", DAY);
+        let count = solve(s, distance, threshold);
+        assert_eq!(count, amount);
     }
 }
